1fd33ee75e83459dd84e80b87744785817dbbe90
Michael Hazell <michaelhazell@hotmail.com>
2021-05-24 23:57:33 -0400

    Various improvements * Cleaned up the JavaScript, slightly * Moved JavaScript out of the HTML * Added favicon

--- webchat.js
+++ webchat.js
@@ -5,43 +5,67 @@
 
 'use strict';
 
-/** Build iframe src for embedding
- * @return A URL string
+/** Build KiwiIRC link
+ *
+ * Useful for embedding into an iframe
+ * @return {string} A URL string
  *
  * Example:
  * https://kiwiirc.com/nextclient/irc.techtronix.net:+6697/#test
  */
-function build_embed_src() {
+function buildKiwiLink() {
     // Variables
-    const base_url = 'https://kiwiirc.com/nextclient/';
-    const server_url = 'irc.techtronix.net';
-    const server_port = 6697;
-    const server_use_tls = true;
+    const baseUrl = 'https://kiwiirc.com/nextclient/';
+    const ircHost = 'irc.techtronix.net';
+    const ircPort = 6697;
+    const useTls = true;
 
     // Get page URL information
     let url = new URL(window.location);
 
     // Construct the iframe src URL
-    let src = base_url;
-    src += server_url
-    if (server_use_tls) {
-        src += ':+' + server_port;
+    let str = baseUrl;
+    str += ircHost;
+    if (useTls) {
+        str += ':+';
     } else {
-        src += ':' + server_port;
+        str += ':';
     }
+    str += ircPort;
+    str += '/';
     if (url.hash === '') {
-        src += '/' + '#lounge';
+        str += '#lounge';
     } else {
-        src += '/' + url.hash;
+        str += url.hash;
     }
+    return str;
 
-    return src;
+    // For now, we are not returning the template version of this for compat
+    // return `${baseUrl}${ircHost}${useTls ? ':+' : ':'}${ircPort}/${url.hash === '' ? '#lounge' : url.hash}`;
 }
 
 /** Change embed iframe dimensions on window resize
  */
-function change_embed_dimensions() {
+function changeChatFrameDimensions() {
     let embed = document.getElementById('chat-iframe');
     embed.setAttribute('height', window.innerHeight);
     embed.setAttribute('width', window.innerWidth);
 }
+
+// Events to attach to
+window.addEventListener('load', () => {
+    let embed = document.createElement('iframe');
+    embed.setAttribute('id', 'chat-iframe');
+    embed.setAttribute('frameborder', 0);
+    embed.setAttribute('src', buildKiwiLink());
+    document.getElementById('chat').appendChild(embed);
+});
+
+window.addEventListener('load', () => {
+    let url = new URL(window.location);
+    let hash = url.hash;
+    if (hash === '') {
+        hash = '#lounge'
+    }
+    document.title = `Techtronix Webchat: ${hash}`;
+});
